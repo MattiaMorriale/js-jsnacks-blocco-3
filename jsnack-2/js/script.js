524e015ec3995527b66c5c3e93c7b83cdd826bc2
mattia_morriale <mattia.morriale1@gmail.com>
2024-02-12 20:05:16 +0100

    fine della stilizzazione dello snack2

--- jsnack-2/js/script.js
+++ jsnack-2/js/script.js
@@ -1,14 +1,28 @@
-const number = ['1','2','3'];
-const letter = ['a','b','c'];
-const final = [];
+const textDemo = document.querySelector('#text');
 
-function pusher(letter, number, final) {
-    for (let i = 0; i < number.length; i++) {
-        final.push(letter[i]);
-        final.push(number[i]);
+const buttonElement = document.querySelector("#avvia");
+
+setTimeout (function(){
+    textDemo.classList.add('op-1');
+    textDemo.classList.remove('op-0');
+}, 2000)
+
+buttonElement.addEventListener("click",
+function() {
+
+    const number = ['1','2','3'];
+    const letter = ['a','b','c'];
+    const final = [];
+    
+    function pusher(letter, number, final) {
+        for (let i = 0; i < number.length; i++) {
+            final.push(letter[i]);
+            final.push(number[i]);
+        }
     }
-}
+    
+    pusher(letter, number, final)
+    
+    document.getElementById('output').innerHTML = final;
 
-pusher(letter, number, final)
-
-console.log(final);
+})
